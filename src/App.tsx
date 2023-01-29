730937b59cdcbb5e7720c5f06c8fceeea041a560
Adarsh <adarshregmi1@gmail.com>
2023-01-29 22:13:20 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { MultiChatWindow } from "react-chat-engine-advanced"
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <MultiChatWindow style={{ height: "100vh" }} />
+    </div>
+  )
+}
+
+export default App
