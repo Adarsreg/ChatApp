06926ef64fd19c27878f19117a29eeff2f71700e
Adarsh <adarshregmi1@gmail.com>
2023-02-02 18:48:35 +0530

    form added

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
-export function Register(props) {
+export function Register() {
 
 
     return (
-        <div>
-            Register
+        <div className='formcontainer'>
+            <div className='formwrapper'>
+                <form>
+                    <input type="text" placeholder='display name' />
+                    <input type="email" placeholder="email" />
+                    <input type="password" placeholder='password' />
+                    <input type="file" />
+                </form>
+            </div>
         </div>
     )
 }
