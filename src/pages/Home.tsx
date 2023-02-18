fb1d477671dc338252023c3859d420c81a5d1dfc
Adarsh <adarshregmi1@gmail.com>
2023-02-18 12:14:08 +0530

    Added styling to Sidebar Chat .1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,9 +5,10 @@ import { Register } from './Register';
 
 const Home = () => {
     return (
-        <div className='home'>
-            <div className='container'>
-                Home
+        <div className='home' >
+            <div className='container bg-indigo-300 h-screen flex  justify-center items-center' style={{ display: 'flex', flexDirection: 'column' }}>
+                <Sidebar />
+                <Chat />
             </div>
         </div>
     )
