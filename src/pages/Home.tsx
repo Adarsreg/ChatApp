fbcd02d09e4c72901caf4a0b30783628ed3e8a5f
Adarsh <adarshregmi1@gmail.com>
2023-02-18 23:49:51 +0530

    home updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Chat from '../components/Chat';
 import Sidebar from '../components/Sidebar';
 import { Register } from './Register';
 
 const Home = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const handleSidebarToggle = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
     return (
-        <div className='home' >
-            <div className='container bg-indigo-300 h-screen flex  justify-center items-center' style={{ display: 'flex', flexDirection: 'column' }}>
-                <Sidebar />
-                <Chat />
-            </div>
+        <div className='home bg-indigo-300 h-screen flex-row flex items-center justify-center' >
+
+            <Sidebar />
+            <Chat />
         </div>
-    )
+
+    );
 }
 
 export default Home;
